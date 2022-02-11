9de5e53d24306d125f4f6b352f0b6c12a66121df
rahulc29 <rahul29112002@gmail.com>
2022-02-11 16:10:33 +0530

    Implement tail-recursive generalised sum

---
+++ chapter-1/generalised-sum-tailrec.scm
@@ -0,0 +1,10 @@
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (+ result (term a)))))
+  (iter a 0))
+(define (range-sum a b)
+  (define (term x) x)
+  (define (succ x) (+ x 1))
+  (sum term a succ b))
