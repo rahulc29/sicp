9f19aa44658bf313f8f2ca994d9a83e031cda197
rahulc29 <rahul29112002@gmail.com>
2022-02-22 02:28:22 +0530

    Implement n-fold smoothing

---
+++ chapter-1/n-fold-smooth.scm
@@ -0,0 +1,16 @@
+(define (compose f g)
+  (lambda(x) (f (g x))))
+(define (repeated f n)
+  (define (iter k result)
+    (if (= k n)
+	result
+	(iter (+ k 1) (compose result f))))
+  (iter 1 f))
+(define dx 0.00001)
+(define (smooth f)
+  (lambda(x)
+    (/
+      (+ (f (- x dx)) (f x) (f (+ x dx)))
+      3.0)))
+(define (smooth-n f n)
+  ((repeated smooth n) f))
